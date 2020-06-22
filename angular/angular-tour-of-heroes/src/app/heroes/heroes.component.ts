25807ded3edb3b3e39d7d63a09d8faa353e15c21
Ha Doan <ha.doanmanh@gmail.com>
2020-06-23 00:04:07 +0800

    introduction to angular

---
+++ angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../Hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero: Hero;
+
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero) {
+    console.log(hero);
+    this.selectedHero = hero;
+  }
+
+}
